aa83a6f9e9aa9a6baf6ed854f7acb995798c53f5
shrey <shrey23kumbhare@gmail.com>
2023-08-20 13:54:29 +0530

    database step1
    #created a database for header menu which is applicable in both desktop and mobile view
    #corrected the link for custom js
    # add personal account to mobile menu

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 //jshint esversion:6
-
+require('dotenv').config()
+const dotenv=require('dotenv');
+require('mongodb')
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 const app = express();
+const findOrCreate = require("mongoose-findorcreate");
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.set('strictQuery', true); mongoose.connect('mongodb://127.0.0.1/webDB');
+mongoose.set('strictQuery', false);
+ 
+// MAIN //
+//HEADER DATABASE//
+const HeaderMenuSchema = new mongoose.Schema ({
+  title: String,
+  sub_title: [{sub_title:String}]
+});
+const headerMenu=  mongoose.model("headerMenu", HeaderMenuSchema);
+const title1=new headerMenu({title:"Men's",sub_title:[{sub_title:"Shirt"},{sub_title:"Shorts & Jeans"},{sub_title:"Safety Shoes"}]});
+const title2=new headerMenu({title:"Women's",sub_title:[{sub_title:"Dress & Frock"},{sub_title:"Earrings"},{sub_title:"Necklace"}]});
+const title3=new headerMenu({title:"Jewelry",sub_title:[{sub_title:"Earrings"},{sub_title:"Couple Rings"},{sub_title:"Necklace"}]});
+const title4=new headerMenu({title:"Perfume",sub_title:[{sub_title:"Clothes Perfume"},{sub_title:"Deodorant"},{sub_title:"Flower Fragrance"}]});
+const defaulttitles=[title1,title2,title3,title4]
+headerMenu.find({}).then(function (founditems) {
+  if(founditems.length == 0){
+  headerMenu.insertMany(defaulttitles)
+  .then(function () {
+    console.log("Successfully saved defult items to DB");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });}
+});
+
+// ROUTES//
 app.get("/",function(req,res){
-  res.render("home");
+  headerMenu.find({}).then(function (founditems) {
+  res.render("home", {titles: founditems});
+  });
 
 });
 app.get("/favouriteItem",function(req,res){
