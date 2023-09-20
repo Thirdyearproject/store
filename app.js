8d978e691121d51d4d84c03607989407a4d79971
shrey <shrey23kumbhare@gmail.com>
2023-09-20 23:17:11 +0530

    developing
    add page
    edit page
    image upload

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
 const findOrCreate = require("mongoose-findorcreate");
+let multer=require('multer')
+let methodOverride = require('method-override')
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'))
 
 mongoose.set('strictQuery', true); mongoose.connect('mongodb://127.0.0.1/webDB');
 mongoose.set('strictQuery', false);
- 
+
 // MAIN //
 //HEADER DATABASE//
 const HeaderMenuSchema = new mongoose.Schema ({
@@ -45,6 +45,40 @@ headerMenu.find({}).then(function (founditems) {
   });}
 });
 
+const pageSchema = new mongoose.Schema({
+  PageUrl : String,
+  PageNavText : String,
+  PageTitle : String,
+  PageMetaDescrition : String,
+  PageMetaKeyword : String,
+  PageHeading : String,
+  PagePhoto : String,
+  PageDetails : String
+  });
+  const  pageModel = mongoose.model('dynamicPages', pageSchema)
+
+//storage and filename
+  let storage = multer.diskStorage({
+    destination: 'public/backend/images/',
+    filename: (req, file, cb) => {
+        //cb(null, Date.now() + file.originalname) // file name setting with current –date
+	cb(null ,  file.originalname) // file name setting with original name
+    }
+})
+
+//IMAGE UPLOAD SETTING
+let upload = multer({
+  storage: storage,
+  // here image validation
+  fileFilter: (req, file, cb) => {
+      if (file.mimetype == "image/jpeg" || file.mimetype == "image/jpg" || file.mimetype == "image/png") {
+          cb(null, true)
+      }
+      else { 
+          cb(null, false)
+          return cb(new Error('Only Jpg, png, jpeg, allow'))
+      }
+  }})  
 // ROUTES//
 app.get("/",function(req,res){
   headerMenu.find({}).then(function (founditems) {
@@ -127,6 +161,122 @@ app.post("/admin/update/:collection/:id", async (req, res) => {
 app.get("/admin/dynamicPage",function(req,res){
   res.render("dynamicPage");
 });
+
+app.get("/admin/navigationPage",function(req,res){
+  pageModel.find({})
+    .then((x)=>{
+        res.render("navigationPage", {x})
+    })
+});
+app.get("/admin/pages/add-pages/",function(req,res){
+  res.render("add-pages");
+});
+app.post("/admin/pages/add-pages/",upload.single('Page_Photo'),function(req,res){
+if(!req.body.page_url){
+  pageModel.findOne({PageUrl :req.body.Page_Url})
+  .then((a)=>{
+      if(a){
+              res.redirect('/admin/navigationPage/') 
+      }
+      else if(!req.file){
+              //create page without photo
+              let data ={
+                  PageUrl : req.body.Page_Url,
+                  PageNavText : req.body.Page_Meta_Description,
+                  PageTitle : req.body.Page_Meta_Keyword,
+                  PageMetaDescrition : req.body.Page_Meta_Description,
+                  PageMetaKeyword : req.body.Page_Meta_Keyword,
+                  PageHeading : req.body.Page_Heading,
+                  //PagePhoto : req.body.file,
+                  PageDetails :req.body.Page_Details
+              }
+              pageModel.create(data)
+              .then((x)=>{
+                  res.redirect('/admin/navigationPage/')
+
+              }).catch((y)=>{
+                  console.log(y)
+              })
+              //end create page
+          
+      }
+      else{
+          //create page with photo
+          let data ={
+              PageUrl : req.body.Page_Url,
+              PageNavText : req.body.Page_Meta_Description,
+              PageTitle : req.body.Page_Meta_Keyword,
+              PageMetaDescrition : req.body.Page_Meta_Description,
+              PageMetaKeyword : req.body.Page_Meta_Keyword,
+              PageHeading : req.body.Page_Heading,
+              PagePhoto : req.file.filename,
+              PageDetails :req.body.Page_Details
+          }
+          pageModel.create(data)
+          .then((x)=>{
+              res.redirect('/admin/navigationPage/')
+
+          }).catch((y)=>{
+              console.log(y)
+          })
+          //end create page
+      }
+  })
+ }
+ else{
+  res.redirect('/admin/navigationPage/')
+ }
+});
+app.get('/admin/pages/edit-pages/:id', (req, res)=>{
+  pageModel.findOne({PageUrl:req.params.id})
+  .then((x)=>{
+      res.render('edit-pages', {x})
+  })
+  .catch((y)=>{
+      console.log(y)
+  })
+})
+
+app.put('/admin/pages/edit-pages/:id', upload.single('Page_Photo'), (req, res)=>{
+  console.log(req.body)
+  if(!req.file){
+    // without image update
+    pageModel.updateOne({PageUrl:req.params.id}, {$set:{
+      PageUrl : req.body.Page_Url,
+      PageNavText : req.body.Page_Meta_Description,
+      PageTitle : req.body.Page_Meta_Keyword,
+      PageMetaDescrition : req.body.Page_Meta_Description,
+      PageMetaKeyword : req.body.Page_Meta_Keyword,
+      PageHeading : req.body.Page_Heading,
+      //PagePhoto : req.body.file,
+      PageDetails :req.body.Page_Details}})
+    .then((x)=>{
+        res.redirect('/admin/navigationPage/')
+    }).catch((y)=>{
+      console.log(y)
+  })
+    //end
+}
+else{
+    // with image update
+    pageModel.updateOne({PageUrl:req.params.id}, {$set:{
+      PageUrl : req.body.Page_Url,
+      PageNavText : req.body.Page_Meta_Description,
+      PageTitle : req.body.Page_Meta_Keyword,
+      PageMetaDescrition : req.body.Page_Meta_Description,
+      PageMetaKeyword : req.body.Page_Meta_Keyword,
+      PageHeading : req.body.Page_Heading,
+      PagePhoto : req.body.file,
+      PageDetails :req.body.Page_Details
+    }})
+    .then((x)=>{
+        res.redirect('/admin/navigationPage/')
+    }).catch((y)=>{
+      console.log(y)
+  })
+}
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
