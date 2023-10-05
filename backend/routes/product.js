888685c2c3b3bdc0dec816d7e0b41b1ab0284bc7
Swarnadip <96777115+Swarnadip-Dey@users.noreply.github.com>
2023-10-05 16:01:55 +0530

    protected routes restricted unauthorized user

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,14 +1,17 @@
 const express=require('express');
 const router=express.Router();
 
-const {getProducts,newProduct, getSingleProduct,updateProduct}=require('../controllers/productController');
+const {getProducts,newProduct, getSingleProduct,updateProduct,deleteProduct}=require('../controllers/productController');
+
+const{isAuthenticatedUser,authorizeRoles}=require('../middlewares/user');
 
 router.route(`/products`).get(getProducts);
 
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser,newProduct);
 
-router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id').put(isAuthenticatedUser,updateProduct)
+                                .delete(isAuthenticatedUser,deleteProduct);
 
 module.exports = router;
